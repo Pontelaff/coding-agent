dc26cae03ebdb01d94a9f1901c56ac1f942291a2
Jonas Gesch <jonas.gesch@gmail.com>
2025-06-28 20:50:59 +0800

    Add verbose print option

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main(args) -> int:
-    if len(args) <= 1:
+    print_verbose = False
+    if len(args) <= 1 or type(args[1]) is not str:
         print("ERROR: Prompt is missing!")
         return 1
-    if len(args) > 2:
+    if len(args) == 3:
+        if args[2] == "--verbose":
+            print_verbose = True
+        else:
+            print("ERROR: Unknown argument.")
+            return 1
+    if len(args) > 3:
         print("ERROR: Too many arguments! Make sure to enclose prompt in quotation marks.")
         return 1
 
     prompt = args[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    print('"' + prompt + '"\n')
-    response = client.models.generate_content(model="gemini-2.0-flash", contents=prompt)
+    if print_verbose:
+        print(f"User prompt: {prompt}\n")
+    response = client.models.generate_content(model="gemini-2.0-flash", contents=messages)
 
     print(response.text)
 
-    print(f"\nPrompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if print_verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     return 0
 
